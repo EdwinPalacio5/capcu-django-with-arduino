fe57958f02af89e37fe6724b0d9ab251ec3953a2
AP16014 <ap16014@ues.edu.sv>
2018-11-21 19:16:48 -0600

    creacion de modelos y configuracion para visualizarlos en el admin

--- apps/proveedor/admin.py
+++ apps/proveedor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.proveedor.models import *
 
 # Register your models here.
+
+admin.site.register (Categoria)
+admin.site.register (Puesto)
+admin.site.register (Proveedor)
+admin.site.register (Control)
--- apps/proveedor/models.py
+++ apps/proveedor/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+class Puesto(models.Model):
+    nombre_puesto = models.CharField(max_length=50)
+    representante_puesto = models.CharField(max_length=50)
+    ubicacion_puesto = models.CharField(max_length=50)
+    categoria_puesto = models.ManyToManyField(Categoria)
+
+    def __str__(self):
+        return self.nombre_puesto
+
+class Proveedor(models.Model):
+	codigo_proveedor = models.CharField(max_length=20)
+	nombre_proveedor = models.CharField(max_length=50)
+	categoria_proveedor = models.ManyToManyField(Categoria)
+	puesto_proveedor = models.ManyToManyField(Puesto)
+
+	def __str__(self):
+		return self.nombre_proveedor
+
+class Control(models.Model):
+	hora_entrada = models.DateTimeField()
+	hora_salida = models.DateTimeField()
+	proveedor = models.ForeignKey (Proveedor, null = True , blank = True , on_delete = models. CASCADE )
+	estado_control = models.BooleanField(default=False)
+
+
